8ffdabd20a5f1426964568c339423c24e751790d
MeoJust <meojust@gmail.com>
2025-01-23 00:42:32 +0300

    stack items

--- _inventory/Inventory.cs
+++ _inventory/Inventory.cs
@@ -1,26 +1,85 @@
 using UnityEngine;
+using System.Collections.Generic; 
 
 public class Inventory : Singleton<Inventory>
 {
     [Header("Config")]
     [SerializeField] int _inventorySize;
-    [SerializeField] InventoryItemSO[] _items;
+    [SerializeField] InventoryItemSO[] _inventoryItems;
 
-    [Header("TEST")] 
+    [Header("TEST")]
     public InventoryItemSO _testItem;
 
     public int InventorySize => _inventorySize;
 
     void Start()
     {
-        _items = new InventoryItemSO[_inventorySize];
+        _inventoryItems = new InventoryItemSO[_inventorySize];
     }
 
-    void Update(){
-        if (Input.GetKeyDown(KeyCode.T)){
-            _items[0] = _testItem.CopyItem();
-            _items[0].Quantity = 10;
-            InventoryUI.Instance.DrawItem(_items[0], 0);
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            AddItem(_testItem, 3);
         }
+    }
+
+    public void AddItem(InventoryItemSO item, int quantity)
+    {
+        if (item == null || quantity <= 0) return;
+        List<int> itemIndexes = CheckItemsStock(item.ItemId);
+        if (item.IsStackable && itemIndexes.Count > 0)
+        {
+            foreach (var index in itemIndexes)
+            {
+                int maxStack = item.MaxStackSize;
+                if (_inventoryItems[index].Quantity < maxStack)
+                {
+                    _inventoryItems[index].Quantity += quantity;
+                    if (_inventoryItems[index].Quantity > maxStack)
+                    {
+                        int dif = _inventoryItems[index].Quantity - maxStack;
+                        _inventoryItems[index].Quantity = maxStack;
+                        AddItem(item, dif);
+                    }
+                    InventoryUI.Instance.DrawItem(_inventoryItems[index], index);
+                    return;
+                }
+            }
+        }
+        int quantityToAdd = quantity > item.MaxStackSize ? item.MaxStackSize : quantity;
+        AddItemToFreeSlot(item, quantityToAdd);
+        int remainingAmount = quantity - quantityToAdd;
+        if (remainingAmount > 0)
+        {
+            AddItem(item, remainingAmount);
+        }
+    }
+
+    void AddItemToFreeSlot(InventoryItemSO item, int quantity)
+    {
+        for (int i = 0; i < _inventorySize; i++)
+        {
+            if (_inventoryItems[i] != null) continue;
+            _inventoryItems[i] = item.CopyItem();
+            _inventoryItems[i].Quantity = quantity;
+            InventoryUI.Instance.DrawItem(_inventoryItems[i], i);
+            return;
+        }
+    }
+
+    List<int> CheckItemsStock(string itemId)
+    {
+        List<int> stock = new List<int>();
+        for (int i = 0; i < _inventoryItems.Length; i++)
+        {
+            if (_inventoryItems[i] == null) continue;
+            if (_inventoryItems[i].ItemId == itemId)
+            {
+                stock.Add(i);
+            }
+        }
+        return stock;
     }
 }
